2114c950cd270ab51895ea82dc3cc5e64e317802
fruitgushers <jeremyadams166@gmail.com>
2023-11-28 12:01:18 -0800

    changed another thing in manifest file

--- app/src/main/java/com/example/truck_food/Review/Menu.java
+++ app/src/main/java/com/example/truck_food/Review/Menu.java
@@ -1,8 +1,10 @@
-package com.example.truck_food;
+package com.example.truck_food.Review;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.truck_food.R;
 
 public class Menu extends AppCompatActivity {
 
