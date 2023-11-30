c71c87dc00620bf7fd6daf3c09322f924d68599c
AnLaxina <anilovlaxina@gmail.com>
2023-11-30 09:45:56 -0800

    Add functionality end activity (Task 5)

--- app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
+++ app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
@@ -2,7 +2,10 @@ package com.example.truck_food.Location;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.truck_food.R;
 
@@ -13,4 +16,24 @@ public class UpdateLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_location);
     }
+
+
+
+    public void mainMenu(View v){
+        // Create an AlertDialog.Builder instance
+        new AlertDialog.Builder(this)
+                .setTitle("Confirmation")
+                .setMessage("Do you want to go back to the main menu?")
+                // If the user confirms, finish the activity
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                // If the user cancels, just close the dialog
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
 }
