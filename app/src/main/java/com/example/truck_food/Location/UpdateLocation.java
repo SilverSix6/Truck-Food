4c35a50a8d4d430e001b6483bf8b63dbb60415fc
AnLaxina <anilovlaxina@gmail.com>
2023-11-30 14:43:33 -0800

    Add feature to find user's current location (Task 5)

--- app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
+++ app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
@@ -1,31 +1,41 @@
 package com.example.truck_food.Location;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.truck_food.R;
-import com.example.truck_food.databinding.ActivityTestLocationBinding;
 import com.example.truck_food.databinding.ActivityUpdateLocationBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class UpdateLocation extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityUpdateLocationBinding binding;
 
+    // Used to ask user for their location
+    private FusedLocationProviderClient fusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         binding = ActivityUpdateLocationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -47,6 +57,8 @@ public class UpdateLocation extends AppCompatActivity implements OnMapReadyCallb
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kelowna, 10.0f));
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        // Asks the user for current location
+        currentLocation(new View(this));
         // Set a click listener for the map
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
@@ -79,5 +91,26 @@ public class UpdateLocation extends AppCompatActivity implements OnMapReadyCallb
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
+
+    public void currentLocation(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request location permissions if they were not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            // Get the last known location
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.clear();
+                        mMap.addMarker(new MarkerOptions().position(currentLocation));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 10.0f));
+                    }
+                }
+            });
+        }
+    }
+
 
 }
