a31f5905deafe3cef2d929e2e8d274bbc6e41241
AnLaxina <anilovlaxina@gmail.com>
2023-11-30 10:08:50 -0800

    Add working Google Maps functionality to Task 5

--- app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
+++ app/src/main/java/com/example/truck_food/Location/UpdateLocation.java
@@ -8,18 +8,46 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.truck_food.R;
+import com.example.truck_food.databinding.ActivityTestLocationBinding;
+import com.example.truck_food.databinding.ActivityUpdateLocationBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class UpdateLocation extends AppCompatActivity {
+public class UpdateLocation extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityUpdateLocationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_location);
+
+
+        binding = ActivityUpdateLocationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        // Create a LatLng object for the location of Kelowna, BC
+        LatLng kelowna = new LatLng(49.8877, -119.4961);
+
+        // Move the camera to Kelowna and zoom in
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kelowna, 10.0f));
     }
 
 
-
-    public void mainMenu(View v){
+    public void mainMenu(View v) {
         // Create an AlertDialog.Builder instance
         new AlertDialog.Builder(this)
                 .setTitle("Confirmation")
