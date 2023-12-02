e688d7cb8a8cc98a3de3c43947a3ce2f2904fae0
SilverSix6 <aidanmorris810@gmail.com>
2023-12-01 21:31:04 -0800

    Revert "Fixed weird bug with saving vendor id in the database"
    This reverts commit a3c80868d7c18448919582cbe7bf3db5451ca317.

--- app/src/main/java/com/example/truck_food/Login/LoginScreen.java
+++ app/src/main/java/com/example/truck_food/Login/LoginScreen.java
@@ -95,7 +95,6 @@ public class LoginScreen extends AppCompatActivity {
                     String username = vendor.getUsername();
                     String password = vendor.getPassword();
                     if(username.equals(EnteredUsername)  && password.equals(EnteredPassword)){
-
                         found = true;
                         account = vendor;
                         accountId = key;
